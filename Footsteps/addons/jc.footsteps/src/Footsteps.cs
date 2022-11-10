c7ca7906aac07284124c3a431b73a69b96cd2ccd
jk7c <jleodevelp@gmail.com>
2022-11-10 17:21:55 -0600

    Improved randomization and cleaned code

--- Footsteps/addons/jc.footsteps/src/Footsteps.cs
+++ Footsteps/addons/jc.footsteps/src/Footsteps.cs
@@ -7,15 +7,12 @@ namespace JC.Footsteps
 	[Tool]
 	public partial class Footsteps: Node3D
 	{
-		// Audio player.
 		public AudioStreamPlayer3D AudioPlayer{ get; set; } = null;
 
-		// Default audio clips.
+		/// <summary> Default footsteps audio streams. </summary>
 		public FootstepsSurfaceAudio DefaultClips{ get; set; } = null;
 
-		// Surfaces.
 		const String kDefaultSurfaceMetaID = "surface";
-
 		String _SurfaceMetaID = kDefaultSurfaceMetaID;
 
 		/// <summary> Surface ID for Metadata. </summary>
@@ -35,21 +32,14 @@ namespace JC.Footsteps
 		[ExportSubgroup("Surface")]
 		[Export] public Array<FootstepsSurface> surfaces;
 
-		// Surface interval.
 		[ExportGroup("Step")]
 
 		/// <summary> Interval between each step. </summary>
 		[Export] public float StepInterval{ get; set; } = 2.0f;
 
-		// FX.
-		// Audio Bus.
-		/// <summary> Index of the audio bus. </summary>
 		public uint BusIndex{ get; set; } = 1;
 
-		// Enable pan fx.
 		bool _EnablePan = false;
-
-		/// <summary> Enable pan FX </summary>
 		public bool EnablePan
 		{
 			get => _EnablePan;
@@ -60,14 +50,9 @@ namespace JC.Footsteps
 			}
 		}
 
-		// Pan FX index.
-		/// <summary> Index of the pan fx. </summary>
 		public uint PanIndex{ get; set; } = 0;
 
-		//
 		bool _EnablePitch = false;
-
-		/// <summary> Enable pitch fx. </summary>
 		public bool EnablePitch
 		{
 			get => _EnablePitch;
@@ -78,11 +63,12 @@ namespace JC.Footsteps
 			}
 		}
 
-		/// <summary> Index of the pitch fx. </summary>
 		public uint PitchIndex{ get; set; } = 1;
+		
+		// Character body node.
+		CharacterBody3D _Character = null;
 
-		// Step.
-		bool _OnAirStore = false;
+		bool  _IsOnAir = false;
 		float _Step;
 		float _DistanceTravelled;
 
@@ -90,17 +76,14 @@ namespace JC.Footsteps
 		AudioStream _CurrentLandingClip = null;
 		Texture _CurrentSurfaceTexture = null;
 
-		// Nodes.
-		CharacterBody3D _Character = null;
 		RandomNumberGenerator _Random = new RandomNumberGenerator();
-
-		//
-		bool _DefaultSurface = true;
-		float _PhysicsDelta;
+		bool _IsDefaultSurface = true;
+		
 		float _PanRange;
 		float _MinUnitSize,   _MaxUnitSize;
 		float _MinPitchRange, _MaxPitchRange;
-		bool _EnablePanner = false;
+		
+		bool _PannerSwitch = false;
 
 		public override void _EnterTree()
 		{
@@ -112,7 +95,7 @@ namespace JC.Footsteps
 			if(Engine.IsEditorHint())
 				return;
 			
-			_EnablePanner = false;
+			_PannerSwitch = false;
 			Play();
 		}
 
@@ -121,17 +104,16 @@ namespace JC.Footsteps
 			if(Engine.IsEditorHint())
 				return;
 			
-			_PhysicsDelta = (float)delta;
 			if(_Character == null)
 				return;
 			
 			if(!_Character.IsOnFloor())
-				_OnAirStore = true;
+				_IsOnAir = true;
 			
-			if(_Character.IsOnFloor() && _OnAirStore)
+			if(_Character.IsOnFloor() && _IsOnAir)
 			{
 				OnLanding();
-				_OnAirStore = false;
+				_IsOnAir = false;
 			}
 
 			// Step.
@@ -140,7 +122,7 @@ namespace JC.Footsteps
 				_DistanceTravelled += _Character.Velocity.Length() * (float)delta;
 				if(_DistanceTravelled > _Step)
 				{
-					_EnablePanner = true;
+					_PannerSwitch = true;
 					Play();
 					_DistanceTravelled = 0.0f;
 					_Step = StepInterval;
@@ -187,7 +169,7 @@ namespace JC.Footsteps
 			}
 			else
 			{
-				_DefaultSurface = true;
+				_IsDefaultSurface = true;
 				PlayDefaultClips();
 			}
 		}
@@ -197,13 +179,14 @@ namespace JC.Footsteps
 			if(AudioPlayer == null)
 				return;
 			
-			_EnablePanner = false;
+			_PannerSwitch = false;
 			Play();
 			PlayAudioPlayer(_CurrentLandingClip);
 		}
 
 		private void PlayAudioPlayer(AudioStream clip)
 		{
+			_Random.Randomize();
 			float unit = _Random.RandfRange(_MinUnitSize, _MaxUnitSize);
 			AudioPlayer.UnitSize = unit;
 			AudioPlayer.Stream = clip;
@@ -214,7 +197,7 @@ namespace JC.Footsteps
 				AudioEffectPanner fx = (AudioEffectPanner)AudioServer.GetBusEffect((int)BusIndex, (int)PanIndex);
 				if(fx != null)
 				{
-					if(_EnablePanner)
+					if(_PannerSwitch)
 					{
 						fx.Pan = _PanRange - fx.Pan - _PanRange;
 						if(fx.Pan == 0.0f)
@@ -247,13 +230,15 @@ namespace JC.Footsteps
 				{
 					if(surface.Exists(_CurrentSurfaceTexture))
 					{
-						_DefaultSurface = false;
+						_IsDefaultSurface = false;
 						_MinUnitSize = surface.MinUnitSize;
 						_MaxUnitSize = surface.MaxUnitSize;
 						_PanRange = surface.PanRange;
 						_MinPitchRange = surface.MinPitchRange;
 						_MaxPitchRange = surface.MaxPitchRange;
 
+
+						_Random.Randomize();
 						if(surface.landingClips.Count > 0)
 							_CurrentLandingClip = surface.landingClips[
 								_Random.RandiRange(0, surface.landingClips.Count - 1)
@@ -267,7 +252,7 @@ namespace JC.Footsteps
 			}
 			else
 			{
-				_DefaultSurface = true;
+				_IsDefaultSurface = true;
 				PlayDefaultClips();
 
 			}
@@ -284,6 +269,7 @@ namespace JC.Footsteps
 			_MinPitchRange = DefaultClips.MinPitchRange;
 			_MaxPitchRange = DefaultClips.MaxPitchRange;
 			
+			_Random.Randomize();
 			if(DefaultClips.landingClips.Count > 0)
 				_CurrentLandingClip = DefaultClips.landingClips[
 					_Random.RandiRange(0, DefaultClips.landingClips.Count - 1)
